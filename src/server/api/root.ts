6669ea9030561696bf573a8b6fe7cdce3d66bc95
Brad Hamill <brhamill@hamill.com>
2023-03-15 23:39:20 -0600

    finished code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { exampleRouter, topicRouter, noteRouter } from "~/server/api/routers";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +8,8 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
